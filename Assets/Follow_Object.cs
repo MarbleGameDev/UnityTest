c471162527739bdb6448c6d7fce900cb6261e095
Uberpangolin <jolewis@student.csdspartans.org>
2015-07-19 19:56:29 -0400

    Camera Follows Car
    I made it so that the camera is offset a little by where the car is

---
+++ Assets/Follow_Object.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Follow_Object : MonoBehaviour 
+{
+	public GameObject target = null;
+	Vector3 positionOffset = Vector3.zero;
+
+
+	void Start () 
+	{
+		positionOffset = target.transform.position + transform.position; 
+	}
+	
+	
+	void Update () 
+	{
+		if (target != null) 
+		{
+			transform.LookAt (target.transform);
+		}
+		transform.position = target.transform.position + positionOffset;
+		
+	}
+	
+}
