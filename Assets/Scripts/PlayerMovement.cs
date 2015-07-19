46c987942c8fc1668df871456672b73b6a67123e
thejereman13 <jereman13@gmail.com>
2015-07-18 20:39:00 -0400

    Car Movement Work
    Also created the start of environment work.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public float moveSpeed;
+
+	public Vector3 input;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		GetComponent<Rigidbody>().AddForce (input * moveSpeed);
+	}
+}
