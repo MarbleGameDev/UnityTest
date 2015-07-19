2bbada47b36821030f0587d4339d188e69ab0911
thejereman13 <jereman13@gmail.com>
2015-07-18 21:25:17 -0400

    Framework for realistic car movement
    Work on rotation and velocity in directions. No code in directions yet.
    Trig to come.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,17 +3,31 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 	public float moveSpeed;
-
+	public float rotateSpeed;
 	public Vector3 input;
+	public float rotation;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		private float temprotation;
+		rotation = transform.eulerAngles.y;//Rotation from left and right keys
+		transform.Rotate (0, Input.GetAxis ("Horizontal") * rotateSpeedn, 0);
 		input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-		GetComponent<Rigidbody>().AddForce (input * moveSpeed);
+		if (rotation > 270) {
+			temprotation -= 270;
+		} else if (this.rotation > 180) {
+			temprotation -= 180;
+		} else if (this.rotation > 90) {
+			temprotation -= 90;
+		}else{
+			temprotation = rotation;
+		}
+		print (rotation);
+		//GetComponent<Rigidbody> ().AddForce (input * moveSpeed);
 	}
 }
