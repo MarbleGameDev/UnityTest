cd072f33eda8077b9050c40db3f0303cc9612533
Uberpangolin <jolewis@student.csdspartans.org>
2015-08-05 21:19:20 -0400

    Added a nontextured Mountain and worked on collision of the wheels and general objects
    realistic (kinda) A work in Progress as well. Just wanted to save it for
    the night and let jeremy check this shit out.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,27 +7,44 @@ public class PlayerMovement : MonoBehaviour {
 	public float rotation;
 	public float velocity;
 
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.name != "floopy") 
+		{
+			
+			velocity = Input.GetAxis ("Vertical");
+			rotation = Input.GetAxis ("Horizontal");
+		}
+	}
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
+	
 
 	}
 
 	// Update is called once per frame
-	void Update () {
-
-
+	void Update () 
+	{
+	
+				
 		velocity = Input.GetAxis ("Vertical");
 		rotation = Input.GetAxis ("Horizontal");
-		if (velocity != 0) {
+			
+			if (velocity != 0)
+		{
 			float rotateSpeed = 18 * moveSpeed;
 			var distance = velocity * moveSpeed * Time.deltaTime;
 			var turn = rotation * rotateSpeed * Time.deltaTime * velocity;
 			transform.Rotate (0, turn, 0);
 			transform.Translate (Vector3.forward * distance);
 			print (Time.deltaTime);
-
-
+					
 		}
+				
+	
+			
 		if ( transform.eulerAngles.z > 35 & 95 > transform.eulerAngles.z  )
 		{
 			transform.Rotate (0,0,-2);
@@ -39,3 +56,5 @@ public class PlayerMovement : MonoBehaviour {
 
 	}
 }
+
+
